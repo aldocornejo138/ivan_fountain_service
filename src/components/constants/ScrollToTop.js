58380366a15ab159cbee6bccd24a6a36c64e795b
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-10-04 10:39:17 -0700

    update

--- src/components/constants/ScrollToTop.js
+++ src/components/constants/ScrollToTop.js
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export default function ScrollToTop() {
+function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "instant" });
+    window.scrollTo(0, 0);
   }, [pathname]);
+
+  return null;
 }
+
+export default ScrollToTop;
