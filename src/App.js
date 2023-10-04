70e3810314a79c55469cb065f9e7fc47396b05c3
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-10-04 10:02:22 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Riverside from "./components/locations/Riverside.js";
 import Home from "./components/home/Home.js";
+import ScrollToTop from "./components/constants/ScrollToTop.js";
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/fountain-service-Riverside" element={<Riverside />} />
