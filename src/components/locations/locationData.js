db45a3ca6d9a97afd3fae29fd7fd351a8644a388
Aldahir Cornejo Ambriz <aldocornejo@Aldahirs-MacBook-Pro.local>
2023-11-02 09:55:22 -0700

    update

--- src/components/locations/locationData.js
+++ src/components/locations/locationData.js
@@ -1,15 +1,20 @@
+import Location1 from "./Location1.jpg";
+import Location2 from "./Location2.jpg";
+import Location3 from "./Location3.jpg";
+import Location4 from "./Location4.jpg";
+
 const imageSlide2 = [
   {
-    url: "Location1.jpg",
+    url: Location1,
   },
   {
-    url: "Location2.jpg",
+    url: Location2,
   },
   {
-    url: "Location3.jpg",
+    url: Location3,
   },
   {
-    url: "Location4.jpg",
+    url: Location4,
   },
   {
     url: "Location5.jpg",
